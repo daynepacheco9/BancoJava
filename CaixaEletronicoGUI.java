79890387a2f9a2bf8509d65be4f65900218bea06
Dayne Pacheco <143220608+daynepacheco9@users.noreply.github.com>
2023-11-20 11:04:39 -0300

    add interface g

---
+++ CaixaEletronicoGUI.java
@@ -0,0 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class CaixaEletronicoGUI {
+    private ContaCorrente conta;
+    private ArrayList<Double> transferenciasRecebidas;
+
+
+
+}
