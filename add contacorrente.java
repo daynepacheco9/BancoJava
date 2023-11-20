cc15a6811f627a47b049d4458af9b4fc15467017
Janainafaria <149442178+Janainafaria@users.noreply.github.com>
2023-11-20 10:54:20 -0300

    Update CaixaEletronico.java
    add PJ e alterando o nome

--- add contacorrente.java
+++ add contacorrente.java
@@ -73,4 +73,60 @@ class ContaCorrentePessoaFisica extends ContaCorrente {
         return saldoFuturo;
     }
 }
+class ContaCorrentePessoaJuridica extends ContaCorrente {
+    private String cnpj;
+
+    public ContaCorrentePessoaJuridica(String nome, String cnpj, double saldo) {
+        super(nome, saldo);
+        this.cnpj = cnpj;
+    }
+
+    public String getCNPJ() {
+        return cnpj;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+            System.out.println("Saque realizado com sucesso!");
+            System.out.println("Saldo atual: R$ " + saldo);
+        } else {
+            System.out.println("Saldo insuficiente ou valor inválido para saque.");
+        }
+    }
+
+    @Override
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito realizado com sucesso!");
+            System.out.println("Saldo atual: R$ " + saldo);
+        } else {
+            System.out.println("Valor inválido para depósito.");
+        }
+    }
+
+    @Override
+    public void transferir(ContaCorrente destino, double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+            destino.depositar(valor);
+            System.out.println("Transferência realizada com sucesso!");
+            System.out.println("Saldo atual: R$ " + saldo);
+        } else {
+            System.out.println("Saldo insuficiente ou valor inválido para transferência.");
+        }
+    }
+
+    @Override
+    public double saldoFuturo(ArrayList<Double> transferenciasRecebidas) {
+        double saldoFuturo = saldo;
+        for (Double valor : transferenciasRecebidas) {
+            saldoFuturo += valor;
+        }
+        return saldoFuturo;
+    }
+}
+
 
