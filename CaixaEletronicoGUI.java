4a4ae6876b791e36dbc6ace5ff4e7ee3f44aa4da
Janainafaria <149442178+Janainafaria@users.noreply.github.com>
2023-11-20 11:07:17 -0300

    implementando a interface
    implementando a interface

--- CaixaEletronicoGUI.java
+++ CaixaEletronicoGUI.java
@@ -12,6 +12,75 @@ public class CaixaEletronicoGUI {
     private ContaCorrente conta;
     private ArrayList<Double> transferenciasRecebidas;
 
+    public CaixaEletronicoGUI(ContaCorrente conta, ArrayList<Double> transferenciasRecebidas) {
+        this.conta = conta;
+        this.transferenciasRecebidas = transferenciasRecebidas;
 
+        // Criar e configurar a janela
+        JFrame frame = new JFrame("Caixa Eletrônico");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+
+        // Criar e configurar os componentes
+        JButton sacarButton = new JButton("Sacar");
+        JButton depositarButton = new JButton("Depositar");
+        JButton transferirButton = new JButton("Transferir");
+        JButton saldoFuturoButton = new JButton("Saldo Futuro");
+
+        // Adicionar os ouvintes de ação
+        sacarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double valorSaque = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor para saque:"));
+                conta.sacar(valorSaque);
+                exibirSaldoAtual();
+            }
+        });
+
+        depositarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double valorDeposito = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor para depósito:"));
+                conta.depositar(valorDeposito);
+                exibirSaldoAtual();
+            }
+        });
+
+        transferirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double valorTransferencia = Double
+                        .parseDouble(JOptionPane.showInputDialog("Digite o valor para transferência:"));
+                try {
+                    ContaCorrente destino = escolherDestinoTransferencia(conta);
+                    conta.transferir(destino, valorTransferencia);
+                    exibirSaldoAtual();
+                } catch (TransferenciaException ex) {
+                    JOptionPane.showMessageDialog(null, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        saldoFuturoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double saldoFuturo = conta.saldoFuturo(transferenciasRecebidas);
+                JOptionPane.showMessageDialog(null, "Saldo Futuro: R$ " + saldoFuturo);
+            }
+        });
+
+        // Configurar o layout
+        JPanel panel = new JPanel();
+        panel.add(sacarButton);
+        panel.add(depositarButton);
+        panel.add(transferirButton);
+        panel.add(saldoFuturoButton);
+
+        // Adicionar o painel à janela
+        frame.getContentPane().add(panel);
+
+        // Exibir a janela
+        frame.setVisible(true);
+    }
 
 }
